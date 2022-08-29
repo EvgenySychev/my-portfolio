146750d9482df439ce4e8457971a7fe6a7033ba8
EvgenySychev <samara808@yandex.ru>
2022-08-29 22:14:45 +0400

    Preloader refactor

--- src/preloader/Preloader.tsx
+++ src/preloader/Preloader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import preloaderGif from "../assets/gif/preloader small.gif";
 import style from "./Preloader.module.scss"
+import {Particle} from "../particle/Particle";
 
 type PreloaderPropsType = {
     callBack: () => void
@@ -14,26 +15,27 @@ export const Preloader = (props: PreloaderPropsType) => {
 
     return (
         <div className={style.preloaderBlock}>
+            <Particle/>
             <span className={style.description}>
                 portfolio in progress...
                 <img src={preloaderGif} alt="preloader..."/>
             </span>
             <div className={style.button} onClick={switchHandler}>
                 <span className={style.animatedBtn}>
-                            <span>
-                                <em>L</em>
-                                <em>o</em>
-                                <em>o</em>
-                                <em>k</em>
-                                <em> </em>
-                                <em>a</em>
-                                <em>n</em>
-                                <em>y</em>
-                                <em>w</em>
-                                <em>a</em>
-                                <em>y</em>
-                            </span>
-                        </span>
+                    <span>
+                        <em>L</em>
+                        <em>o</em>
+                        <em>o</em>
+                        <em>k</em>
+                        <em> </em>
+                        <em>a</em>
+                        <em>n</em>
+                        <em>y</em>
+                        <em>w</em>
+                        <em>a</em>
+                        <em>y</em>
+                    </span>
+                </span>
             </div>
         </div>
     )
