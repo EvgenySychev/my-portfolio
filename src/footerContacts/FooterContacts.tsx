52019caf7685bdb5196e541cc9bf4743d638b4b0
EvgenySychev <samara808@yandex.ru>
2022-08-30 22:38:01 +0400

    header contact refactor

--- src/footerContacts/FooterContacts.tsx
+++ src/footerContacts/FooterContacts.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import s from './FooterContacts.module.scss'
 
-
 export const FooterContacts = () => {
+
     return (
         <div className={s.footer}>
             <div className={s.socials}>
-                <a className={s.vk} href={'https://t.me/easychev'}>t</a>
-                <a className={s.insta} href={''}>Insta</a>
+                <a className={s.telegram} href={'https://t.me/easychev'}>Telegram</a>
                 <a className={s.git} href={'https://github.com/EvgenySychev'}>GitHub</a>
             </div>
         </div>)
