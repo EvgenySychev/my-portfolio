f93f2041c419c2d4c482201b0607990e42ee4fbb
EvgenySychev <samara808@yandex.ru>
2022-08-30 19:43:17 +0400

    added the ability to download the CV

--- src/header/downloadCV/DownloadCV.tsx
+++ src/header/downloadCV/DownloadCV.tsx
@@ -1,9 +1,13 @@
 import style from "./DownloadCV.module.scss"
 
+// @ts-ignore
+import myPDF from "../../assets/pdf/resume-SychevEA-Frontend-Developer.pdf"
+
 export const DownloadCV = () => {
+
     return (
 
-        <a className={style.downloadCvBlock}>
+        <a href={myPDF} download={"resume-SychevEA-Frontend-Developer.pdf"} className={style.downloadCvBlock}>
             <span className={style.animatedButton}>
                 <span>DOWNLOAD CV</span>
             </span>
