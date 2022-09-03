b08ac83cd6c0bbf432a634c4d9ea5c561c555a3c
EvgenySychev <samara808@yandex.ru>
2022-09-03 20:29:34 +0400

    refactor footer styles on hover

--- src/footerContacts/FooterContacts.tsx
+++ src/footerContacts/FooterContacts.tsx
@@ -6,8 +6,12 @@ export const FooterContacts = () => {
     return (
         <div className={s.footer}>
             <div className={s.socials}>
-                <a className={s.telegram} href={'https://t.me/easychev'}>Telegram</a>
-                <a className={s.git} href={'https://github.com/EvgenySychev'}>GitHub</a>
+                <a href={'https://t.me/easychev'}>
+                    <span>Telegram</span>
+                </a>
+                <a href={'https://github.com/EvgenySychev'}>
+                    <span>GitHub</span>
+                </a>
             </div>
         </div>)
 }
