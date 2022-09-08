a4254751f24bf5e234dcd8253f1a78ce1ab43502
EvgenySychev <samara808@yandex.ru>
2022-09-08 21:46:01 +0400

    add description to my Achievements

--- src/achivments/Achievements.tsx
+++ src/achivments/Achievements.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import style from './Achievements.module.scss'
 import {Title} from "../common/Components/title/Title";
 import {Achievement} from "./achievement/Achievement";
+import TypingEffect from "new-react-typing-effect";
 
 type AchievementItemsType = {
     description: string
@@ -10,11 +11,9 @@ type AchievementItemsType = {
 export const Achievements = () => {
 
     const achievementDescription: AchievementItemsType[] = [
-        {description: 'tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la om-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom'},
-        {description: 'tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la om-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom'},
-        {description: 'tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la om-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom'},
-        {description: 'tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la om-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom'},
-        {description: 'tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la om-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom tra-ta-ta la-la-la boom-boom-boom'},
+        {description: 'По результатам работы в 2021 г. лучший отдел продаж автомобилей в группе компаний «АСПЭК-Авто»'},
+        {description: 'Преодолел отметку в 1200 проданных новых автомобилей'},
+        {description: 'Успешно завершил обучение по программе «Мастер продаж», являюсь сертифицированным консультантом по продаже автомобилей'},
     ]
 
     return (
@@ -26,9 +25,25 @@ export const Achievements = () => {
                 <div className={style.achievements}>
                     {achievementDescription
                         .map((t, i) => (
-                            <Achievement title={`Achievement № ${i + 1}`} description={t.description}/>
+                            <Achievement title={`Достижение № ${i + 1}`} description={t.description}/>
                         ))}
                 </div>
+                <div className={style.description}>
+                    <TypingEffect
+                        messages={["Здесь ещё есть место :)"]}
+                        cursor="|"
+                        textRenderer={(text, renderedCursor) => {
+                            return <p>{text}{renderedCursor}</p>
+                        }}
+                        options={{
+                            text: {
+                                charactersPerSecond: 20,
+                                fullTextDelayMS: 2000,
+                            },
+                        }}
+                    />
+                </div>
+
             </div>
         </div>
     );
