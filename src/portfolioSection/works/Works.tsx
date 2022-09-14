23a4cd9132a599e6dc36b5f4dd15ba664951bb18
EvgenySychev <samara808@yandex.ru>
2022-09-14 22:12:54 +0400

    add button "GitHub" and "open" on Work

--- src/portfolioSection/works/Works.tsx
+++ src/portfolioSection/works/Works.tsx
@@ -7,11 +7,14 @@ import todoListImg from './../../assets/image/Todolist.jpeg'
 import counterImg from './../../assets/image/counter.png'
 import cardsImg from './../../assets/image/cards.jpg'
 import {BackgroundImageType} from "../../App";
+// @ts-ignore
+import Tilt from 'react-tilt'
 
 type workItemType = {
     title: string
     worksImg: BackgroundImageType
     linkToWork: string
+    linkToGit: string
     description: string
 }
 
@@ -22,24 +25,28 @@ export const Works = () => {
             title: 'Counter',
             worksImg: {backgroundImage: `url(${counterImg})`},
             linkToWork: 'https://EvgenySychev.github.io/counter',
+            linkToGit: 'https://github.com/EvgenySychev/counter',
             description: 'Мой первый проект на React. Использовал функциональные компоненты. Сделал верстку. Реализовал логику работы приложения. Работа с Localstorage. Для эффективного хранения данных приложения использовал библиотеку Redux'
         },
         {
             title: 'Todolist',
             worksImg: {backgroundImage: `url(${todoListImg})`},
             linkToWork: 'https://EvgenySychev.github.io/TO-DO_LIST',
+            linkToGit: 'https://github.com/EvgenySychev/TO-DO_LIST',
             description: 'Учебный проект. Написан с использованием функциональных компонент. Так же использовалась библиотека Material UI. Разработка приложения через тестирование TDD. Хранение состояние приложения с помощью Redux. Запросы на сервер с помощью библиотеки Axios. Разработка формы логинизации, валидация формы с помощью formik. Обработка ошибок запросов на сервер'
         },
         {
             title: 'Social network',
             worksImg: {backgroundImage: `url(${socialImg})`},
             linkToWork: '',
+            linkToGit: '',
             description: 'В разработке'
         },
         {
             title: 'Cards',
             worksImg: {backgroundImage: `url(${cardsImg})`},
             linkToWork: '',
+            linkToGit: '',
             description: 'В разработке'
         }
     ]
@@ -52,8 +59,13 @@ export const Works = () => {
                 <div className={style.works}>
                     {workItems
                         .map(t => (
-                            <Work style={t.worksImg} title={t.title} description={t.description}
-                                  linkToWork={t.linkToWork}/>
+                                <Work
+                                    style={t.worksImg}
+                                    title={t.title}
+                                    description={t.description}
+                                    linkToWork={t.linkToWork}
+                                    linkToGit={t.linkToGit}
+                                />
                         ))
                     }
                 </div>
--- src/portfolioSection/works/work/Work.tsx
+++ src/portfolioSection/works/work/Work.tsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import s from './Work.module.scss';
+// @ts-ignore
+import Tilt from 'react-tilt'
 
 export type WorkType = {
     style: any
     title: string
     description: string
     linkToWork: string
+    linkToGit: string
 }
 
-export function Work({linkToWork, style, description, title}: WorkType) {
+export function Work({linkToWork, style, description, title, linkToGit}: WorkType) {
     return (
         <div className={s.work}>
-
-            <a href={linkToWork}>
-                <div className={s.image} style={style}/>
-            </a>
+                <Tilt className="Tilt" options={{max: 15, scale:1}}>
+                    <div className={s.image} style={style}>
+                        <div className={s.socials}>
+                            <a href={linkToWork}>
+                                <span>открыть</span>
+                            </a>
+                            <a href={linkToGit}>
+                                <span>GitHub</span>
+                            </a>
+                        </div>
+                    </div>
+                </Tilt>
             <div className={s.projectInfo}>
                 <h3 className={s.header}> {title} </h3>
                 <span className={s.description}> {description} </span>
