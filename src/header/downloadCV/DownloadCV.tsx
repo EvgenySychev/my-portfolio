665e02ac3f9af3cf3905e826d75cbb2717ab0180
EvgenySychev <samara808@yandex.ru>
2022-09-30 23:22:08 +0400

    change CV pdf

--- src/header/downloadCV/DownloadCV.tsx
+++ src/header/downloadCV/DownloadCV.tsx
@@ -1,12 +1,12 @@
 import style from "./DownloadCV.module.scss"
 
 // @ts-ignore
-import myPDF from "../../assets/pdf/resume-SychevEA-Frontend-Developer.pdf"
+import myPDF from "../../assets/pdf/Resume_Evgeny_Sychev_Frontend-developer.pdf"
 
 export const DownloadCV = () => {
 
     return (
-        <a href={myPDF} download={"resume-SychevEA-Frontend-Developer.pdf"} className={style.downloadCvBlock}>
+        <a href={myPDF} download={"Resume_Evgeny_Sychev_Frontend-developer.pdf"} className={style.downloadCvBlock}>
             <span className={style.animatedButton}>
                 <span>DOWNLOAD CV</span>
                 <div>CV</div>
