1ffd6932f95ee81e8d99e8c2ef2f325daac75162
EvgenySychev <samara808@yandex.ru>
2023-05-29 17:57:22 +0400

    added slider in BackgroundImg.tsx

--- src/common/components/BackgroundImg.tsx
+++ src/common/components/BackgroundImg.tsx
@@ -1,4 +1,4 @@
-import s from "../../App.module.scss";
+import s from './BackgroundImg.module.scss';
 import frontImg1 from "../../assets/image/firstFoto.jpg";
 import frontImg2 from "../../assets/image/secondFoto.jpg";
 import frontImg3 from "../../assets/image/SychFull.jpg";
@@ -6,33 +6,60 @@ import {useEffect, useState} from "react";
 
 export const BackgroundImg = () => {
 
-    // const arrImg = [frontImg1, frontImg2, frontImg3]
-    const [counter, setCounter] = useState(1)
+    const arrImg = [
+        {
+            id: 1,
+            imgUrl: frontImg1
+        },
+        {
+            id: 2,
+            imgUrl: frontImg2
+        },
+        {
+            id: 3,
+            imgUrl: frontImg3
+        }
+    ]
+    const [backgroundImg, setBackgroundImg] = useState(arrImg)
+    const [currentIndex, setCurrentIndex] = useState(0)
 
     useEffect(() => {
-        setInterval(() => {
-            setCounter((prevIndex) =>
-                prevIndex === 3 ? 1 : prevIndex + 1)
-        }, 3000)
-    }, [])
+        const lastIndex = backgroundImg.length - 1
+        if (currentIndex < 0) {
+            setCurrentIndex(lastIndex)
+        }
+        if (currentIndex > lastIndex) {
+            setCurrentIndex(0)
+        }
+    }, [currentIndex, backgroundImg])
+
+    useEffect(() => {
+        let slider = setInterval(() => {
+            setCurrentIndex((prevIndex) =>
+                prevIndex === 2 ? 0 : prevIndex + 1)
+        }, 15000)
+        return () => clearInterval(slider)
+    }, [currentIndex])
 
     return <div>
-        <div className={s.backgroundImg}
-            style={{
-                backgroundImage: `url(${frontImg1})`,
-                display: counter === 1 ? '' : 'none'
-            }}/>
-        <div className={s.backgroundImg}
-            style={{
-                backgroundImage: `url(${frontImg2})`,
-                display: counter === 2 ? '' : 'none'
-            }}/>
-        <div className={s.backgroundImg}
-            style={{
-                backgroundImage: `url(${frontImg3})`,
-                display: counter === 3 ? '' : 'none'
-            }}/>
+        {backgroundImg.map((currentImg, imgIndex: number) => {
+            // const {id,imgUrl} = currentImg
+            let position = 'nextSlide'
+            if (imgIndex === currentIndex) {
+                position = 'activeSlide'
+            }
+            if (imgIndex === currentIndex - 1 || (currentIndex === 0 && backgroundImg.length - 1)) {
+                position = 'lastSlide'
+            }
+            return (
+                <div className={position}>
+                    <div className={s.backgroundImg}
+                         style={{
+                             backgroundImage: `url(${backgroundImg[currentIndex].imgUrl})`,
+                         }}/>
+                </div>
+            )
+        })}
     </div>
-}
 
-//https://medium.com/suyeonme/react-how-to-build-an-image-slider-with-hooks-in-react-decd4a3ab152
+}
